58c98815126c8e6cff091b54ec75395414d1b7ed
greemaltend4 <dengestabiy@hotmail.com>
2025-09-01 10:39:18 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'API d\'Intégration Cloud Série!');
+});
+
+// Routes pour les intégrations des services cloud
+// Ajouter ici les routes pour Google Drive, Dropbox, AWS S3
+
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
